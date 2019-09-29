12ae38047107ad674c6cdfe882ae9a1b9adb0978
Murillo Andre Maleski <murillomaleski@gmail.com>
2019-09-29 13:32:01 -0300

    Explode Blocks OK
    Bomba agora explode blocks destutíveis e aparece uma animação de explosão.

---
+++ Assets/Scripts/DestroyBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBlock : MonoBehaviour
+{
+    public GameObject blockExplode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Explosion")
+        {
+            GameObject bExp = Instantiate(blockExplode, transform.position, transform.rotation);
+            Destroy(bExp, 0.9f);
+            gameObject.SetActive(false);
+            Destroy(gameObject, .3f);
+        }
+    }
+
+}
